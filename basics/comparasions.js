eb1d5ba9d4506821b736e6e2d02f50a0dda69cbe
RISHABH SIKARWAR <141947007+rishabh-sikarwar@users.noreply.github.com>
2023-10-31 16:34:59 +0000

    comparisions commit

---
+++ basics/comparasions.js
@@ -0,0 +1,15 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);   //true  and all other are false
+console.log(undefined >0);
+console.log(undefined ==0);
+console.log(undefined >= 0);
+
+
+
+
+
+console.log("2"===2);  // strict check not only checks value but also checkes datatypes aslo
+// console.log();
+// console.log();
+// console.log();
