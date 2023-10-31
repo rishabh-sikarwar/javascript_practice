117ea06eaf32ed1502849db75f91a9046c9a5b2b
RISHABH SIKARWAR <141947007+rishabh-sikarwar@users.noreply.github.com>
2023-10-31 16:48:55 +0000

    datatypes and completed 1:56 hour

---
+++ basics/datatypes.js
@@ -0,0 +1,19 @@
+// two types of datatypes   
+//    1.  Primitive   
+//         7 types   : string, number, boolean , null , undefined , symbol, bigint
+
+
+//       2. Reference (non primitive)
+//         Array, Objects , Functions
+
+
+
+//    javascript dynamically or statically typed language  ???               dynamically typed as type of variable is not defined
+const id= Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);     //false
+
+
+
+
