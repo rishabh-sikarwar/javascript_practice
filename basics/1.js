37ca86adb864e9716140ad79273e5dbdb8aa0324
RISHABH SIKARWAR <141947007+rishabh-sikarwar@users.noreply.github.com>
2023-10-31 16:26:32 +0000

    commited till 1:30 length

--- basics/1.js
+++ basics/1.js
@@ -17,3 +17,14 @@ console.log(valueInNumber);
 //   to convert in  boolean    " Boolean(hshs)"
 //    empty string   in boolean false      and if added some value in the string then it comes true
 
+
+
+
+                             // *****************************operators**********************************************
+
+console.log("2"+1);                             
+console.log( 1 + "4");                             
+console.log("100"+ 2+2 );   
+console.log(1+2+ "3");   
+
+//  so which ever comes first the operation will work on that basis only
