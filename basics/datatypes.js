ca1b36ebdc529dc77d02d8197b9307546269d8f0
RISHABH SIKARWAR <141947007+rishabh-sikarwar@users.noreply.github.com>
2023-11-01 14:39:11 +0000

    further learning

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -17,3 +17,28 @@ console.log(id === anotherId);     //false
 
 
 
+// ******************************************************************************************************************
+// let is a primitive type  
+// stack  (Primitive)  :;  Heap (Non - Primitive )    it is a reference type    copy nahi di jati hai 
+
+let myName = "rishabh"
+let mysurname = myName      //value nahi di jaati balki ek copy di jati hai 
+mysurname = "sikarwar"
+
+console.log(myName)
+console.log(mysurname)
+
+
+let userOne = {
+    email: "rishabhsikarwar1000@gmail.com" ,
+    section : "first"
+}
+
+let userTwo = userOne
+
+userTwo.email = "asdf@gmail.com"
+
+console.log(userOne.email);     //isme bhi value change ho gae hai 
+console.log(userTwo.email);
+
+
