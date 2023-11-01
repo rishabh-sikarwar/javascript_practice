57172588a16027a297213e10511d2ada3d61d5e5
RISHABH SIKARWAR <141947007+rishabh-sikarwar@users.noreply.github.com>
2023-11-01 14:56:55 +0000

    comiit

---
+++ basics/strings.js
@@ -0,0 +1,10 @@
+const name = "Rishabh"
+const repoCount =  6
+
+console.log(name + repoCount + "Very less need to improve");     //not recommended for modern times 
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`)        // in this type backticks is used which is a modern way ( ` ` )
+
+
+// another way to declare a string 
+const gameName = new String('superMario')
