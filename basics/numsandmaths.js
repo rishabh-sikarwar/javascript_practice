d089065085c914e22ed24c47d30e7d50665219d7
RISHABH SIKARWAR <141947007+rishabh-sikarwar@users.noreply.github.com>
2023-11-02 16:03:56 +0000

    time stamp 3:11

--- basics/numsandmaths.js
+++ basics/numsandmaths.js
@@ -40,12 +40,33 @@ console.log(Math.floor(Math.random() *(max-min + 1) + min))
 // *******************************************  Date and Time  *****************************************************************
 
 let myDate = new Date()
-console.log(myDate);                                  //date is a object in js
+console.log(myDate);                                  //date is an object in js
 console.log(myDate.toString());
 
 let myCreateDate = new Date(2023,0,23,5,3)
 console.log(myCreateDate.toLocaleString());
 
+let properDate = new Date("01-12-2022")
+console.log(properDate.toLocaleString());
+
+
+
+// *********************to see the time stamp ***********************************
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreateDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);                              //to get the date of taday
+
+console.log(newDate.getDay());                       //start with monday   
+console.log(newDate.getMonth());                      //start with zero in january
+newDate.toLocaleString('default' , {
+    weekday: "long",
+})
 
 
 
