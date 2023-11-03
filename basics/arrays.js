d4d311b0e28ffd760be6b8bc46f67a2b17145a60
RISHABH SIKARWAR <141947007+rishabh-sikarwar@users.noreply.github.com>
2023-11-03 10:37:24 +0000

    time stamp 3:45

--- basics/arrays.js
+++ basics/arrays.js
@@ -33,10 +33,38 @@ console.log(abcd);
 
 const marvel_heros = ["thor", "ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
-const all_heros = marvel_heros.push(dc_heros)
+const all_heros = marvel_heros.concat(dc_heros)
 console.log(all_heros);
-console.log(all_heros[3][1]);
+console.log(all_heros[3]);
 
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
+const all_new_heros = [ ...marvel_heros, ...dc_heros]        //spread operator another method to use instead of concat
 console.log(all_new_heros);
+
+
+const another_array = [2,4,5, [2,4,6], 6,[2,4, [3,6,7,8]]]
+
+const real_another_array= another_array.flat(Infinity)                   //flat gives the single array and remove the arrays which are inside the single array
+
+console.log(real_another_array);
+
+
+console.log(Array.isArray("Rishabh"));                                  //not an array
+
+console.log(Array.from("Rishabh"));                                    //convert into an array
+console.log(Array.from({name: "Rishabh"}));                  //interesting       gives an empty array   if it can not figure out
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2 , score3));                 //of returns array from given points
+
+
+
+
+
+
+
+
