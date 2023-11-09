d538ff5036e1e428af69bcc2580875b18dbc62ee
RISHABH SIKARWAR <141947007+rishabh-sikarwar@users.noreply.github.com>
2023-11-09 15:58:55 +0000

    time stamp 5:55

---
+++ basics/IIFE.js
@@ -0,0 +1,17 @@
+// Immediately Invoked Function Expressions   (IIFE)
+
+(function chai(){
+    console.log("Data Base Connected");
+
+// })();                            main important is the semicolone to end the iife 
+
+//            ()()    first parenthesis is the function defination and the second one is the execution call 
+
+
+
+
+// *************************************Or  as both are same *********************************************
+
+(  (argument ) =>  {
+    console.log( `DB connected two ${argument } `);
+}  )("pass the parameter");
