5e3d7e4c50a2f67ce1701a203c631ec56d912c35
RISHABH SIKARWAR <141947007+rishabh-sikarwar@users.noreply.github.com>
2023-11-09 15:41:45 +0000

    time stamp 5:48

--- basics/arrow_functions.js
+++ basics/arrow_functions.js
@@ -19,3 +19,42 @@ function chai(){
  chai()
 
 
+// const chai = function() {
+//     let username = "Rishabh "
+//     console.log(this.username);
+// } 
+
+// chai()
+
+
+
+const chai = () => {                           //arrow function   () => {}
+    let username = "Rishabh "
+    console.log(this);
+} 
+
+chai()
+// ***************************************example of arrow function *************************************
+
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
+
+console.log(addTwo(3,5));
+
+
+// ******************************inplicit of arrow functions******************************************
+                                // explicit matlab aapko return lagana pad raha hai 
+
+const addThree = (num3 , num4) => num3 + num4
+
+const addThreee = (num3 , num4) => (num3 + num4)            //return is necessary only in curly braces but not in any other case
+                                                        //   and parentesis is necessary to return an object so ( ) it is good 
+
+
+
+
+
+
+
+
