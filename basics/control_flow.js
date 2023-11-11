8a21694828957ccc29a601071430ea0e139d3282
RISHABH SIKARWAR <141947007+rishabh-sikarwar@users.noreply.github.com>
2023-11-11 15:55:43 +0000

    video 25 time stamp 17:00

--- basics/control_flow.js
+++ basics/control_flow.js
@@ -6,4 +6,33 @@
   //2 === "2"             //false  check the datatype also it is in strict checking
     //!==                 //strict checking in anti pattern 
 
+
+
+
+
+
+
+
+    // const score = 200 
+
+    // if(score > 100){
+    //   const power = "fly" 
+      
+    //   console.log(User power: ${power});
+
+    // }    
+    // console.log(User power: ${power});
+
+
+
+
+
+    // *************************************short hand notation *****************************************
+    const balance = 1000
+    if (balance > 500) console.log("test");          //in this we can not write multiple lines 
+    if (balance > 500) console.log("test"),
+    console.log("test2");                             //by this we can write multiple lines code   we can use comma
+
     
+
+
